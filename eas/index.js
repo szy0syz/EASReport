419711493f92f7b1dfe03ad70c38d97d4fa65143
szy0syz <szy0syz@gmail.com>
2017-02-10 22:54:48 +0800

    test

--- eas/index.js
+++ eas/index.js
@@ -1,1 +1,4 @@
 var Sequelize = require('Sequelize');
+var sequelize = new Sequelize("mssql://szy0syz0yngf2017:xQnWdw3u4BOgwTuU@192.168.97.199:1433/YNNZ2011001_20160912");
+
+sequelize.import('./models')
