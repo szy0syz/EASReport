538057ac232fdad9cecb92ac13637c13bf82a3e2
szy0syz <szy0syz@gmail.com>
2017-02-11 00:44:04 +0800

    test

--- eas/index.js
+++ eas/index.js
@@ -1,8 +1,7 @@
 var Sequelize = require('Sequelize');
 var sequelize = new Sequelize("mssql://szy0syz0yngf2017:xQnWdw3u4BOgwTuU@192.168.97.199:1433/YNNZ2011001_20160912");
-// sequelize.options.timestamps = false;
-var T_BAS_BillType = sequelize.import('./models/T_BAS_BillType')
-console.log(T_BAS_BillType.options.timestamps);
-T_BAS_BillType.findAll().then(function(rows) {
-    console.log(rows[0].FName_l2);
+
+var T_BAS_BillType = sequelize.import('./models/T_BD_PaymentType')
+T_BAS_BillType.findAll({limit: 10}).then(function(rows) {
+    console.log(rows);
 })
