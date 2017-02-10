5c1140d7c7f3e77fb4814c9b134b0c56df9e3d7b
szy0syz <szy0syz@gmail.com>
2017-02-11 01:39:22 +0800

    test associations

--- eas/index.js
+++ eas/index.js
@@ -1,7 +1,20 @@
 var Sequelize = require('Sequelize');
 var sequelize = new Sequelize("mssql://szy0syz0yngf2017:xQnWdw3u4BOgwTuU@192.168.97.199:1433/YNNZ2011001_20160912");
 
-var T_BAS_BillType = sequelize.import('./models/T_BD_PaymentType')
-T_BAS_BillType.findAll({limit: 10}).then(function(rows) {
-    console.log(rows);
+var T_IM_SaleIssueEntry = sequelize.import('./models/T_IM_SaleIssueEntry');
+var T_BD_Customer = sequelize.import('./models/T_BD_Customer');
+T_BD_Customer.hasMany(T_IM_SaleIssueEntry);
+T_IM_SaleIssueEntry
+  .findAll({
+    where: {
+      FID: {$in: ["//+EZEEHQPa9cg9Rl1X+RbvAf74=","//0koJx+T06P5VxHU+YuWrvAf74="]}
+    },
+    include: [{
+      model: T_BD_Customer,
+      as: "c",
+      where: {FID: sequelize.col('FCustomerID')}
+    }]
+  })
+  .then(function(rows) {
+    console.log(rows.length);
 })
