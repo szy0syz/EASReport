e0f72b70420db27a0f5f152f7fa9e63cefd6d33d
szy0syz <szy0syz@gmail.com>
2017-02-11 12:13:27 +0800

    left join OK

--- eas/index.js
+++ eas/index.js
@@ -2,17 +2,34 @@ var Sequelize = require('Sequelize');
 var sequelize = new Sequelize("mssql://szy0syz0yngf2017:xQnWdw3u4BOgwTuU@192.168.97.199:1433/YNNZ2011001_20160912");
 
 var T_IM_SaleIssueEntry = sequelize.import('./models/T_IM_SaleIssueEntry');
-var T_BD_Customer = sequelize.import('./models/T_BD_Customer');
-T_BD_Customer.hasMany(T_IM_SaleIssueEntry);
+var T_IM_SaleIssueBill = sequelize.import('./models/T_IM_SaleIssueBill');
+
+//T_IM_SaleIssueEntry.belongsToMany(T_IM_SaleIssueBill, {as: 'sb', through:'as.FID'})
+
+T_IM_SaleIssueBill.hasMany(T_IM_SaleIssueEntry, 
+  {
+    as: 'se',
+    foreignKey: 'FParentID',
+    constraints: false,
+  });
+
+T_IM_SaleIssueEntry.belongsTo(T_IM_SaleIssueBill, 
+  {
+    as: 'sb',
+    foreignKey: 'FID',
+    constraints: false,
+  });
+
 T_IM_SaleIssueEntry
   .findAll({
     where: {
       FID: {$in: ["//+EZEEHQPa9cg9Rl1X+RbvAf74=","//0koJx+T06P5VxHU+YuWrvAf74="]}
     },
     include: [{
-      model: T_BD_Customer,
-      as: "c",
-      where: {FID: sequelize.col('FCustomerID')}
+      model: T_IM_SaleIssueBill,
+      as: "sb",
+      required : false
+      //,where: {FID: sequelize.col('FCustomerID')}
     }]
   })
   .then(function(rows) {
