9e58a4ece7220d306e929e5c0d651989b5da7f1c
szy0syz <szy0syz@gmail.com>
2017-02-15 13:54:06 +0800

    彻底改造算法完成！

--- eas/index.js
+++ eas/index.js
@@ -51,45 +51,11 @@ Object.defineProperty(Array.prototype, 'group', {
 });
 /////////////
 
-
-
-Promise.join(query, queryCurtAcc ,queryLastAcc ,function(curtData, curtAccData, lastAccData){
-  console.log(curtData.length + '___' + curtAccData.length + '___' + lastAccData.length);
-});
-
-query.then(function(res) {
-  var rept = {
-    sumQty: 0,
-    sumAmount: 0,
-
-    sumNQty: 0,
-    sumNAmount: 0,
-
-    sumUreaQty: 0,
-    sumUreaAmount: 0,
-
-    sumPQty: 0,
-    sumPAmount: 0,
-
-    sumKQty: 0,
-    sumKAmount: 0,
-
-    sumNPKQty: 0,
-    sumNPKAmount: 0,
-
-    sumNPKhQty: 0,
-    sumNPKhAmount: 0
-
-  }
-
-  var array1 = res.group(item => item.FBrandCarbaMind);
-  
+function statFert(arrData) {
   //以后这里从数据库取，不要写死！
   var brandFString = ['尿素','碳铵','硫铵','氯化铵','硝磷铵','普钙','重钙','钙镁','磷铵','富钙','硫酸钾','氯化钾','硫酸钾肥','高含量','低含量']
   var brandF = [];
-
-  var brandC = accObj = [];
-
+  //根据顺序初始化
   brandFString.forEach(function(s,i) {
     var o = {
       name: s,
@@ -98,8 +64,8 @@ query.then(function(res) {
     };
     brandF.push(o);
   })
-
-  res.forEach(function(item, index) {
+  //根据数据分析
+  arrData.forEach(function(item, index) {
     brandF.forEach(function(brand,i) {
       if(item.FBrandFertilizer == brand.name) {
         brand.sumQty += item.FBaseQty;
@@ -107,49 +73,39 @@ query.then(function(res) {
       }
     });
   });
-
   brandF.push({
     name: '复合肥',
     sumQty: brandF[13].sumQty+ brandF[14].sumQty,
     sumAmount: brandF[13].sumAmount+ brandF[14].sumAmount,
-  })
-  
-  ///////////////////////
-  var newarr1 = res.filter(function(v) { return v.FBrandCarbaMind != '非尿素'}).group(ii => ii.FMaterialType3);
-  var testarr = [1,2,3,4,5].reduce((acc, val) => { return acc + val}, 0);
+  });
+  //返回结果
+  return brandF;
+}
+
+function statUrea(arrDate) {
+  var brandC = accObj = [];
+  var newarr1 = arrDate.filter(function(v) { return v.FBrandCarbaMind != '非尿素'}).group(ii => ii.FMaterialType3);
   newarr1.forEach(function(v1, i1) {
     var o = {
       name: v1.key,
       sumQty: 0,
       sumAmount: 0
     }
-    //console.log(v1.data.length); //reduce(function(pv, cv) { return pv + cv; }, 0);
-    //o.sumQty = v1.data.reduce((acc, val) => { return acc + val.FBaseQty }, 0);
-    //o.sumAmount = v1.data.reduce((acc, val) => { return acc + val.FAmount }, 0);
     // 一次循环求两个字段的和
     accObj = v1.data.reduce((acc, val) => {
       acc.sumQty += val.FBaseQty;
       acc.sumAmount += val.FAmount; 
       return acc;
-    }, {sumQty:0, sumAmount: 0});//初始化acc对象！
+    }, {sumQty:0, sumAmount:0});//初始化acc对象！
     o.sumQty = accObj.sumQty;
     o.sumAmount = accObj.sumAmount;
     brandC.push(o);
   });
+  return brandC;
+}
 
-  console.log(brandF)
+Promise.join(query, queryCurtAcc ,queryLastAcc ,function(curtData, curtAccData, lastAccData){
+  console.log(curtData.length + '___' + curtAccData.length + '___' + lastAccData.length);
+  var sum1 = statFert(curtData);
+  var sim2 = statUrea(curtData);
 });
-
-
-// // ---no~~~
-// function getData() {
-//   var data;
-//   return query.then(function(result) {
-//     data = result;
-//   }).return(data);
-// }
-
-//     .then(function(rows) {
-//         console.log(Object.prototype.toString.call(rows[0])); //array
-//         console.log(rows.length);
-//     })
