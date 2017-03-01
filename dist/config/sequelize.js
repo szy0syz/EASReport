8af16f15248d3e29a2a91a6d697f570bc6d81af9
szy0syz <szy0syz@gmail.com>
2017-03-02 01:22:51 +0800

    删除babel

--- dist/config/sequelize.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Sequelize = require('Sequelize');
-var config = require('./config');
-
-module.exports = function () {
-  var seq = new Sequelize(config.dbConnectStr, config.sequelizeOptions);
-  return seq;
-};
--- dist/db/sqlConditions.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var sqlConditions = {
-    FBizDateStart: '2017-02-04',
-    FBizDateEnd: '2017-02-04'
-};
-
-module.exports = sqlConditions;
