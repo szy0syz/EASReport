b6463c44d6996fdfc7c41ff6d4908525b899c4ec
szy0syz <szy0syz@gmail.com>
2017-03-29 17:06:00 +0800

    更新express配置

--- src/app.js
+++ src/app.js
@@ -10,6 +10,18 @@ app.get('/report/daily', function (req, res) {
   console.log("_________55555");
 })
 
+//倒数第二处理500
+app.use(function(err, req, res, next) {
+  console.log('检查到未处理的错误：' + err.message);
+  res.sned('500 - 服务器错误');
+})
+
+//最后处理404
+app.use(function(req, res) {
+  console.log('未找到路由');
+  res.send('404 - 未找到');
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
