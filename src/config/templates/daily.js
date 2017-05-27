4abc3a87994c73220665380f9185d8888094a8be
szy0syz <szy0syz@gmail.com>
2017-05-27 11:30:50 +0800

    * 修正打印模板

--- src/config/templates/daily.js
+++ src/config/templates/daily.js
@@ -47,14 +47,15 @@ function printSaleSummary(statRes, startDate) {
 function printSaleAccSummary(statRes, startDate) {
   let res = "三、" + startDate.toString().slice(0,4) + "年累计销售：" + statRes.sumAccCurtQty.toFixed(decimalDigits) + "吨，同比增长" 
     + (((statRes.sumAccCurtQty - statRes.sumAccLastQty) / statRes.sumAccLastQty) * 100).toFixed() + "%；累计销额"
-    + (statRes.sumAccCurtAmount / 10000).toFixed(decimalDigits) + "万元，同比增长" + (((statRes.sumAccCurtAmount - statRes.sumAccLastAmount) / statRes.sumAccLastAmount) * 100).toFixed() + '%；'
+    + (statRes.sumAccCurtAmount / 10000).toFixed(decimalDigits) + "万元，同比增长" + (((statRes.sumAccCurtAmount - statRes.sumAccLastAmount) / statRes.sumAccLastAmount) * 100).toFixed() + '%；其中'
     + statRes.subAcc + '('
     + statRes.subAccDetail.reduce((acc, val) => {
       acc.push(val.name + val.sum.toFixed() + '吨,同比' + val.ratio + '%')
       return acc
     }, []).join(';')
     + ')；' 
-    + statRes.jckAcc;
+    + statRes.jckAcc
+    + '。';
   return res;
 }
 
